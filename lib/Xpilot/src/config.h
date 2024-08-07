ff0d502c648da7a4804c919582079789f8212c58
Jamal Meizongo <mrmeizongo@outlook.com>
2024-08-07 19:48:01 -0400

    Updated airplane type selection description in README

--- lib/Xpilot/src/config.h
+++ lib/Xpilot/src/config.h
@@ -39,9 +39,9 @@ Flight stabilization software
  * Directions are airplane-wise i.e. looking down the nose
  * FULL_PLANE: Has ailerons(1 or 2 channel), elevator and rudder.
  * FULL_PLANE_V_TAIL: Has ailerons(1 or 2 channel) and v tail deflectors. Left V tail deflector goes to elevator ouput, right goes to rudder output.
- * RUDDER_ELEVATOR_ONLY: Only rudder and elevator. They go to their respective channels.
  * FLYING_WING_RUDDER: Has 2 independent ailerons and a rudder. The aileron I/O is mixed with elevator I/O(elevon). Has rudder control
  * FLYING_WING_NO_RUDDER: Only has 2 independent ailerons. The aileron I/O is mixed with elevator I/O(elevon).
+ * RUDDER_ELEVATOR_ONLY: Only rudder and elevator. They go to their respective channels.
  * RUDDER_ELEVATOR_ONLY_V_TAIL: No ailerons, left V tail deflector goes to elevator, right goes to rudder.
  */
 #define FULL_PLANE
