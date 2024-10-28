cf70080bc9c0e113c3dbbfaad9141b53ae157d25
Jamal Meizongo <mrmeizongo@outlook.com>
2024-10-28 08:36:41 -0400

    Comment Correction

--- lib/PID/src/PID.cpp
+++ lib/PID/src/PID.cpp
@@ -98,8 +98,8 @@ int16_t PID::Compute(float currentError)
         else
             derivative = (currentError - previousError) / deltaTime;
 
-        // Riscrete low pass filter, cuts out the
-        // high frequency noise that can drive the controller crazy
+        // Discrete low pass filter
+        // Cuts out the high frequency noise that can drive the controller crazy
         float RC = 1 / (2 * M_PI * fCut);
         derivative = previousDerivative +
                      ((deltaTime / (RC + deltaTime)) *
