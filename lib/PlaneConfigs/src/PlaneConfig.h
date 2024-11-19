98034685fc0c3e80bbd9f2d5cf71f19b0e0d6429
Jamal Meizongo <mrmeizongo@outlook.com>
2024-11-19 10:56:39 -0500

    Started tracking PlaneConfig and PlaneSelector

--- lib/PlaneConfigs/src/PlaneConfig.h
+++ lib/PlaneConfigs/src/PlaneConfig.h
@@ -1,32 +1,50 @@
+// 11/18/2024 by Jamal Meizongo (mrmeizongo@outlook.com)
+// This and other library code in this repository
+// are partial releases and work is still in progress.
+// Please keep this in mind as you use this piece of software.
+
+/* ============================================
+Flight stabilization software
+    Copyright (C) 2024 Jamal Meizongo (mrmeizongo@outlook.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+===============================================
+*/
 #ifndef _PLANE_CONFIG_H
 #define _PLANE_CONFIG_H
 #include "PlaneSelector.h"
 
-/*
- * Use conditional compilation based on airplane definitions in PlaneSelector
- * Exmample
- * #if defined(USE_GLIDER) || defined(USE_CUB) || defined(USE_ACRO)
- * #error Only one airplane need to be defined at a time
- * #endif
- *
- * #if defined(USE_GLIDER)
- * #include "Glider.h"
- * #undef USE_GLIDER
- * #elif defined(USE_CUB)
- * #include "Cub.h"
- * #undef USE_CUB
- * #elif defined(USE_ACRO)
- * #include "Acro.h"
- * #undef USE_ACRO
- * #else
- * #define USE_DEFAULT
- * #endif
- */
+#if defined(USE_SAM_1000) && defined(USE_GFOX)
+#error Cannot use more than one config
+#endif
 
-/*
- * NOTE
- * If you intend to modify and use only the default settings in config.h you can leave this file as is
- */
-#define USE_DEFAULT
+#if defined(USE_SAM_1000)
+#include "Sam1000.h"
+#undef USE_SAM_1000
+#elif defined(USE_GFOX)
+#include "GFox.h"
+#undef USE_GFOX
+#else
+#include "DefaultConfig.h"
+#endif
 
 #endif // PLANE_CONFIG_H
