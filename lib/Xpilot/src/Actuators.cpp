d8cdf8fbb917f4455a6060fc3ca1d6e329d8a02b
Jamal Meizongo <mrmeizongo@outlook.com>
2024-11-20 20:25:09 -0500

    Refactored actuators

--- lib/Xpilot/src/Actuators.cpp
+++ lib/Xpilot/src/Actuators.cpp
@@ -9,31 +9,34 @@ Actuators::Actuators(void)
 void Actuators::init(void)
 {
     // Set up output servos
-    for (uint8_t channel = AILERON1; channel < NUM_CHANNELS; channel++)
-        controlServo[channel].attach(channel, SERVO_MIN_PWM, SERVO_MAX_PWM);
+    // This assumes consecutive numbering of channels and output pins
+    for (uint8_t ch = AILERON1, pin = AILPIN1_OUTPUT; ch < NUM_CHANNELS; ch++, pin++)
+    {
+        controlServo[ch].attach(pin, SERVO_MIN_PWM, SERVO_MAX_PWM);
+    }
 }
 
 void Actuators::setServoOut(const int16_t (&SRVout)[NUM_CHANNELS])
 {
-    for (uint8_t i = 0; i < NUM_CHANNELS; i++)
+    for (uint8_t ch = AILERON1; ch < NUM_CHANNELS; ch++)
     {
-        channelOut[i] = SRVout[i];
+        channelOut[ch] = SRVout[ch];
     }
 }
 
-int16_t Actuators::getServoOut(Channel channel)
+int16_t Actuators::getServoOut(Channel ch)
 {
-    if (channel < 0 || channel >= NUM_CHANNELS)
+    if (ch < AILERON1 || ch >= NUM_CHANNELS)
         return -1;
 
-    return channelOut[channel];
+    return channelOut[ch];
 }
 
 void Actuators::writeServos(void)
 {
-    for (uint8_t i = 0; i < NUM_CHANNELS; i++)
+    for (uint8_t ch = AILERON1; ch < NUM_CHANNELS; ch++)
     {
-        controlServo[i].writeMicroseconds(channelOut[i]);
+        controlServo[ch].writeMicroseconds(channelOut[ch]);
     }
 }
 
