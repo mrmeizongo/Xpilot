11559a3afbd5d1a51734b7abf313f8cfec30cbbd
Jamal Meizongo <mrmeizongo@outlook.com>
2025-08-11 14:20:51 -0400

    Readded config and selector

---
+++ lib/PlaneConfigs/src/PlaneSelector.h
@@ -0,0 +1,46 @@
+// 11/19/2024 by Jamal Meizongo (mrmeizongo@outlook.com)
+// This and other library code in this repository
+// are partial releases and work is still in progress.
+// Please keep this in mind as you use this piece of software.
+
+/* ============================================
+Flight stabilization software
+    Copyright (C) 2024 Jamal Meizongo (mrmeizongo@outlook.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+===============================================
+*/
+
+#ifndef _PLANE_SELECTOR_H
+#define _PLANE_SELECTOR_H
+
+// Example use
+// Uncomment the plane config you want to use
+/*
+#define USE_GLIDER
+#define USE_SAILPLANE
+*/
+
+#define USE_RAVEN
+// #define USE_SAM1000
+
+#endif // _PLANE_SELECTOR_H
