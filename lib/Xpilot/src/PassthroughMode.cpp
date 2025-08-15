5db00274fd3439887cce6914e71b573990599bee
Jamal Meizongo <mrmeizongo@outlook.com>
2025-08-15 11:18:43 -0400

    Using first order filter to increase performance

--- lib/Xpilot/src/PassthroughMode.cpp
+++ lib/Xpilot/src/PassthroughMode.cpp
@@ -1,9 +1,9 @@
 #include "Mode.h"
 #include <LowpassFilter.h>
 
-static SecondOrderLPF<int16_t> rollLPF{PT_LPF_FREQ};
-static SecondOrderLPF<int16_t> pitchLPF{PT_LPF_FREQ};
-static SecondOrderLPF<int16_t> yawLPF{PT_LPF_FREQ};
+static FirstOrderLPF<int16_t> rollLPF{PT_LPF_FREQ};
+static FirstOrderLPF<int16_t> pitchLPF{PT_LPF_FREQ};
+static FirstOrderLPF<int16_t> yawLPF{PT_LPF_FREQ};
 static unsigned long previousTime = 0;
 
 void PassthroughMode::enter(void)
