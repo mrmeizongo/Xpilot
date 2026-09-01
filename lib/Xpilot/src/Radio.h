fa70c4c96b6fc16a1e301a207eeae74e9cc230f4
Jamal Meizongo <mrmeizongo@outlook.com>
2026-09-01 08:14:07 -0400

    Revised min/max default radio pwm

--- lib/Xpilot/src/Radio.h
+++ lib/Xpilot/src/Radio.h
@@ -38,13 +38,12 @@ Flight stabilization software
 #include "SystemConfig.h"
 #include "FlightConfigAccess.h"
 
-constexpr int16_t RX_PWM_MIN = 700;   // Lowest pwm expected from transmitter
-constexpr int16_t RX_PWM_TRIM = 1500; // Trim pwm expected from transmitter
-constexpr int16_t RX_PWM_MAX = 2300;  // Highest pwm expected from transmitter
-
-constexpr int16_t RX_FAILSAFE_PWM = 1746;      // My rx PWM on signal loss
+constexpr int16_t RX_PWM_MIN = 600;            // Lowest pwm expected from transmitter
+constexpr int16_t RX_PWM_TRIM = 1500;          // Trim pwm expected from transmitter
+constexpr int16_t RX_PWM_MAX = 2400;           // Highest pwm expected from transmitter
+constexpr int16_t RX_FAILSAFE_PWM = 1746;      // My rx PWM output for all channels on signal loss
 constexpr uint8_t RX_FAILSAFE_TOLERANCE = 10;  // Tolerance used for determining a failsafe condition
-constexpr int16_t RX_TIMEOUT_MS = 100;         // Rx timeout; 5 missed PWM frames triggers a failsafe
+constexpr int16_t RX_TIMEOUT_MS = 110;         // Rx timeout; 5 missed 22ms PWM frames triggers a failsafe
 constexpr int16_t RX_3_SW_POS_THRESHOLD = 466; // 3 position switch input separator
 
 enum CHANNELMASK : uint8_t
