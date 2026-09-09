3553c653241b2869a141e37b335e3b67cd97e480
Jamal Meizongo <mrmeizongo@outlook.com>
2026-09-09 13:52:44 -0400

    Miscellaneous changes

--- lib/Xpilot/src/Radio.h
+++ lib/Xpilot/src/Radio.h
@@ -37,9 +37,9 @@ Flight stabilization software
 #include "SystemConfig.h"
 #include "FlightConfigAccess.h"
 
-constexpr int16_t RX_PWM_MIN = 600;            // Lowest pwm expected from transmitter
+constexpr int16_t RX_PWM_MIN = 600;            // Lowest valid pwm expected from transmitter
 constexpr int16_t RX_PWM_TRIM = 1500;          // Trim pwm expected from transmitter
-constexpr int16_t RX_PWM_MAX = 2400;           // Highest pwm expected from transmitter
+constexpr int16_t RX_PWM_MAX = 2400;           // Highest valid pwm expected from transmitter
 constexpr int16_t RX_FAILSAFE_PWM = 1746;      // My rx PWM output for all channels on signal loss
 constexpr uint8_t RX_FAILSAFE_TOLERANCE = 10;  // Tolerance used for determining a failsafe condition
 constexpr int16_t RX_TIMEOUT_MS = 110;         // Rx timeout; 5 missed 22ms PWM frames triggers a failsafe
