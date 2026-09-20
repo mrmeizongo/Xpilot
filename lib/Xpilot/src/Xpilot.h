60fca9de35d5eef1509add231ec8745eb528ffed
Jamal Meizongo <mrmeizongo@outlook.com>
2026-09-20 09:40:27 -0400

    Miscellaneous changes

--- lib/Xpilot/src/Xpilot.h
+++ lib/Xpilot/src/Xpilot.h
@@ -83,6 +83,7 @@ private:
 
     // This is the state of the flight stabilization system
     Mode* currentMode;
+
     void updateFlightMode(void);
 
     // Task handlers for the scheduler to manage periodic tasks
