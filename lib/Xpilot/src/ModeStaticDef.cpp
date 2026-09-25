52cbca4b436e9fe6d13515a98d62e9c4260b147a
Jamal Meizongo <mrmeizongo@outlook.com>
2026-09-24 21:49:24 -0400

    Miscellaneous changes

--- lib/Xpilot/src/ModeStaticDef.cpp
+++ lib/Xpilot/src/ModeStaticDef.cpp
@@ -5,24 +5,24 @@
  * We define them here instead
  */
 
-int32_t Mode::input_trpy[4]{0, 0, 0, 0};
-int16_t Mode::output_trpy[4]{0, 0, 0, 0};
+int32_t Mode::input_trpy[4]{};
+int16_t Mode::output_trpy[4]{};
 
-AirplaneMixer::Outputs Mode::mixerOutputs{0, 0, 0, 0};
+AirplaneMixer::Outputs Mode::mixerOutputs{};
 
-int32_t Mode::imu_rpy[3]{0, 0, 0};
-int32_t Mode::imu_g[3]{0, 0, 0};
+int32_t Mode::imu_rpy[3]{};
+int32_t Mode::imu_g[3]{};
 
-int16_t Mode::SRVout[Actuators::CHANNEL::CHANNEL_COUNT]{0, 0, 0, 0, 0};
+int16_t Mode::SRVout[Actuators::CHANNEL::CHANNEL_COUNT]{};
 
-PIDF<int32_t, int16_t> Mode::rollPIDF;
-PIDF<int32_t, int16_t> Mode::pitchPIDF;
-PIDF<int32_t, int16_t> Mode::yawPIDF;
+PIDF<int32_t, int16_t> Mode::rollPIDF{};
+PIDF<int32_t, int16_t> Mode::pitchPIDF{};
+PIDF<int32_t, int16_t> Mode::yawPIDF{};
 
-AirplaneMixer Mode::airplaneMixer;
+AirplaneMixer Mode::airplaneMixer{};
 
-SlewRateLimiter<int32_t> Mode::rollSlew;
-SlewRateLimiter<int32_t> Mode::pitchSlew;
-SlewRateLimiter<int32_t> Mode::yawSlew;
+SlewRateLimiter<int32_t> Mode::rollSlew{};
+SlewRateLimiter<int32_t> Mode::pitchSlew{};
+SlewRateLimiter<int32_t> Mode::yawSlew{};
 
-int16_t Mode::flaperonInput = 0;
+int16_t Mode::flaperonInput{};
